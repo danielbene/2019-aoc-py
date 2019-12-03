5b0eb09b92460eb82f04b57fbb355cda8734bbf0
unknown <danielbene@protonmail.com>
2019-12-03 20:33:46 +0100

    Fix incorrections

--- src/day2_2.py
+++ src/day2_2.py
@@ -5,27 +5,40 @@ inputFile = iohandler.begin(__file__)
 # --- solution ---
 
 intcodeArray = list(map(int, inputFile.readline().split(',')))
-position = 0
 
-intcodeArray[1] = 12
-intcodeArray[2] = 2
 
-while position <= len(intcodeArray):
-    if intcodeArray[position] == 99:
-        break
-    else:
-        if intcodeArray[position] == 1:
-            intcodeArray[intcodeArray[position + 3]] = \
-                intcodeArray[intcodeArray[position + 1]] + intcodeArray[intcodeArray[position + 2]]
+def calc(pos_a: int, pos_b: int):
+    tmp_array = intcodeArray.copy()
+    position = 0
+    tmp_array[1] = pos_a
+    tmp_array[2] = pos_b
+
+    while position <= len(tmp_array):
+        if tmp_array[position] == 99:
+            break
         else:
-            intcodeArray[intcodeArray[position + 3]] = \
-                intcodeArray[intcodeArray[position + 1]] * intcodeArray[intcodeArray[position + 2]]
-    position += 4
+            if tmp_array[position] == 1:
+                tmp_array[tmp_array[position + 3]] = \
+                    tmp_array[tmp_array[position + 1]] + tmp_array[tmp_array[position + 2]]
+            else:
+                tmp_array[tmp_array[position + 3]] = \
+                    tmp_array[tmp_array[position + 1]] * tmp_array[tmp_array[position + 2]]
+        position += 4
 
-# this is a bit cheesy, but the rocket researches show that position 1 value increases output value by 256k,
-# and position 2 value increases that by 1
-noun = intcodeArray[1] + int(float((19690720 - intcodeArray[0]) / 256000))
-verb = intcodeArray[2] + int(float((19690720 - intcodeArray[0]) % 256000))
+    return tmp_array[0]
+
+
+baseValue = 19690720 - calc(12, 2)
+nounInc = (calc(2, 0) - calc(1, 0))
+verbInc = (calc(0, 2) - calc(0, 1))
+
+if nounInc > verbInc:
+    noun = 12 + int(float(baseValue / nounInc))
+    verb = 2 + int(float(baseValue % nounInc / verbInc))
+else:
+    verb = 2 + int(float(baseValue / verbInc))
+    noun = 12 + int(float(baseValue % verbInc / nounInc))
+
 output = 100 * noun + verb
 
 # --- solution ---
