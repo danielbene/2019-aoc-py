4a007ad3e4cd559742e3406bf448ff87e297c2e6
Bene Dániel <bene.daniel@ibcontroll.hu>
2019-12-05 09:57:40 +0100

    Add solution to day3 part 1

--- src/day3_1.py
+++ src/day3_1.py
@@ -4,21 +4,50 @@ inputFile = iohandler.begin(__file__)
 
 # --- solution ---
 
-# two wires, one per input lines
-# find the closest intersection to the central port
-# directions: R,L,U,D, and the number represent the distance/steps in that direction
-# we need the CLOSEST intersection TO THE CENTRAL if multiple are present
-# distance is the sum of the x,y axis so like 3+3=6
-# they start from the SAME POINT somewhere in the grid
-
-grid = [[], []]
 redWire = inputFile.readline().split(',')
 blueWire = inputFile.readline().split(',')
 
+redWireCoords = []
+blueWireCoords = []
 
 
-print(redWire)
-print(blueWire)
+def calculate_path(input, output):
+    global redWireCoords, blueWireCoords
+    current_position = [0, 0]
+
+    for step in input:
+        if step[0] == 'R':
+            mod = [1, 0]
+        elif step[0] == 'L':
+            mod = [-1, 0]
+        elif step[0] == 'U':
+            mod = [0, 1]
+        else:
+            mod = [0, -1]
+
+        for i in range(int(step[1:])):
+            coord = [current_position[0] + mod[0], current_position[1] + mod[1]]
+            output.append(tuple(coord))
+            current_position = coord
+
+
+calculate_path(redWire, redWireCoords)
+calculate_path(blueWire, blueWireCoords)
+
+intersections = list(set(redWireCoords).intersection(blueWireCoords))
+manhattans = []
+
+for intersection in intersections:
+    intersection = list(intersection)
+
+    if intersection[0] < 0:
+        intersection[0] *= -1
+
+    if intersection[1] < 0:
+        intersection[1] *= -1
+
+    manhattans.append(intersection[0] + intersection[1])
+
 # --- solution ---
 
-iohandler.end(str(''))
+iohandler.end(str(min(manhattans)))
