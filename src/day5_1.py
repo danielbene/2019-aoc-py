86ac4d2f49a7177b43a8385975402c8c4dfa6035
unknown <danielbene@protonmail.com>
2019-12-07 14:55:41 +0100

    Add final solution to day 5 part 1

--- src/day5_1.py
+++ src/day5_1.py
@@ -4,30 +4,10 @@ inputFile = iohandler.begin(__file__)
 
 # --- solution ---
 
-# 1 - three param - addition
-# 2 - three param - multiplication
-
-# two new opcode instruction 3 and 4
-# 3 single param -> takes an int from input (stdin?) and saves to the position defined by the param
-# 4 single param -> outputs the value to the address by the param
-
-# parameter modes
-# 0 -> as before param shows the position to get the value from
-# 1 immediate mode -> param represent value so 50 is 50
-# eg: 11002 -> ABCDE (A mode of 3rd param, B mode of 2nd param, C mode of 1st param, DE opcode)
-
-
-# parameters that an istruction writes to will never be in immediate mode?!!!
-# instruction pointer should be incremented according to opcode param num
-# ints can be negative
-
-# input should be stdin (always 1 input), probably part 2 will use it differently
-# output shows the state of the test, not 0 means thers an error
-#   - final output is the answer (if every other is 0)
-
 intcodeArray = list(map(int, inputFile.readline().split(',')))
 position = 0
 usrInput = 0
+diagCode = 0
 
 
 def get_param_modes(value):
@@ -47,59 +27,37 @@ def first_mode(position_num) -> int:
     return int(intcodeArray[position_num])
 
 
-def calculate(param_mode_array, opcode):
-    if param_mode_array[2] == 0:
-        second = zero_mode(position + 1)
-    else:
-        second = first_mode(position + 1)
+def get_param_value_by_mode(param_mode_array):
+    first = zero_mode(position + 1) if param_mode_array[2] == 0 else first_mode(position + 1)
+    second = zero_mode(position + 2) if param_mode_array[1] == 0 else first_mode(position + 2)
+    # this is different, because we need only the position num to the array element assignment
+    third = first_mode(position + 3) if param_mode_array[0] == 0 else position + 3
 
-    if param_mode_array[1] == 0:
-        third = zero_mode(position + 2)
-    else:
-        third = first_mode(position + 2)
+    return [first, second, third]
 
-    if opcode == 1:
-        if param_mode_array[0] == 0:
-            intcodeArray[first_mode(position + 3)] = second + third
-        else:
-            intcodeArray[position + 3] = second + third
-    elif opcode == 2:
-        if param_mode_array[0] == 0:
-            intcodeArray[first_mode(position + 3)] = second * third
-        else:
-            intcodeArray[position + 3] = second * third
-    else:
-        print('----------------FAIL1----------------')
+
+def calculate(opcode_value, opcode):
+    params = get_param_value_by_mode(get_param_modes(opcode_value))
+    intcodeArray[params[2]] = params[0] + params[1] if opcode == 1 else params[0] * params[1]
 
 
 while position <= len(intcodeArray):
-    value = intcodeArray[position]
-    if value == 99:
-        print('BREAK')
+    opcode_value = intcodeArray[position]
+    if opcode_value == 99:
         break
 
-    opcode = get_int_pos(value, -1)
-    param_modes = get_param_modes(value)
-
-    print(value, opcode, param_modes)
-    print(intcodeArray)
+    opcode = get_int_pos(opcode_value, -1)
 
     if opcode == 1 or opcode == 2:
-        calculate(param_modes, opcode)
-    elif opcode == 3:
-        intcodeArray[first_mode(position + 1)] = int(input('Give me input, ya\' filthy bastard: '))
-    elif opcode == 4:
-        print('#######MODE_4#######', intcodeArray[position + 1])
-    else:
-        print('----------------FAIL2----------------', opcode)
-
-    print(intcodeArray)
-
-    if opcode == 1 or opcode == 2:
+        calculate(opcode_value, opcode)
         position += 4
-    else:
+    elif opcode == 3:
+        intcodeArray[first_mode(position + 1)] = 1  # int(input('Give me input, ya\' filthy bastard: '))
+        position += 2
+    elif opcode == 4:
+        diagCode = position + 1
         position += 2
 
 # --- solution ---
 
-iohandler.end(str(''))
+iohandler.end(str(zero_mode(diagCode)))
